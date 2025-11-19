b9b9c6d19c610f230d6767bd67fa52998e543dd5
saeeedhany <alsaeedalbasi0ny@gmail.com>
2025-11-19 16:21:20 +0200

    remove all files!

--- audio/v_trigger.py
+++
@@ -1,6 +0,0 @@
-class VoiceTrigger:
-    def wait_for_trigger(self):
-
-        input("Say 'Hey Makfouf' and press Enter to simulate: ")
-        return True
-
--- camera/camera_inputs.py
+++
@@ -1,6 +0,0 @@
-class CameraInput:
-    def start_stream(self):
-
-        for i in range(3):
-            yield f"frame_{i}"
-
